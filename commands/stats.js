280513446779a05fe85178415013fcc7315c0783
LennyStrideR <kil.oil@yandex.ru>
2018-10-24 10:53:20 +0300

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -5,13 +5,16 @@ module.exports.run = async (bot, message, args) => {
 
   const bname = "Molecular"
   const bid = "490609897176563735"
-
+const njs = process.version;
+const djs = Discord.version;
 
   const embed = new Discord.RichEmbed()
   .setTitle("Stats")
   .addField("● Servers", `${bot.guilds.size}`, true)
   .addField("● Members", `${bot.users.size}`, true)
   .addField("● Text editor", "Atom Editor")
+  .addField("Discord.js", djs)
+  .addField("Node.js", njs)
   .setFooter(`Bot: ${bname}, ID: ${bid}`)
   .setURL("https://discord.io/molecularsupport")
   .addField("● Channels", `${bot.channels.size}`);

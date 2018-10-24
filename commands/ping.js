a4989168796483a25788342b8e0ffe6ba1001996
LennyStrideR <kil.oil@yandex.ru>
2018-10-24 15:54:22 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,16 +2,17 @@ const Discord = require('discord.js')
 
 exports.run = async (bot, message, args) => {
 
-    let start = Date.now(); message.channel.send('Pong! ').then(message => { 
-    let diff = (Date.now() - start); 
-    let API = (bot.ping).toFixed(2)
+    
+    const m = await message.channel.send("Pinging....").then(message => { 
+    let diff = m.createdTimestamp - message.createdTimestamp; 
+    let API = Math.round(bot.ping);
         
         let embed = new Discord.RichEmbed()
         .setTitle(`🔔 Pong!`)
         .setColor("#c294e3")
         .addField("📶 Latency", `${diff}ms`, true)
         .addField("💻 API", `${API}ms`, true)
-        message.edit(embed);
+        m.edit(embed);
       
     });
 

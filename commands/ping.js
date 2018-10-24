86464dbba27d6c9c6fe1fdf816609e8e40f35c88
LennyStrideR <kil.oil@yandex.ru>
2018-10-24 11:45:02 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,10 @@
 fconst Discord = require('discord.js')
 
-exports.run = async (client, message, args, color) => {
+exports.run = async (bot, message, args) => {
 
     let start = Date.now(); message.channel.send(message.channel.id, 'Pong! ').then(message => { 
     let diff = (Date.now() - start); 
-    let API = (client.ping).toFixed(2)
+    let API = (bot.ping).toFixed(2)
         
         let embed = new Discord.RichEmbed()
         .setTitle(`🔔 Pong!`)

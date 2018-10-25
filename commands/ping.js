ef2fe14877d162402e17a2088c6ff11df806d5ca
LennyStrideR <kil.oil@yandex.ru>
2018-10-25 07:53:40 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
 
 
-    let diff = message.createdTimestamp - Date.now();
+    let diff = Date.now() - message.createdTimestamp;
     let API = Math.round(bot.ping);
 
         let embed = new Discord.RichEmbed()

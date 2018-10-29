1e7ba0b238f727baf8b5f4677f0bbb5857a8578a
LennyStrideR <kil.oil@yandex.ru>
2018-10-29 22:19:27 +0300

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -3,6 +3,9 @@ const moment = require("moment");
 
 module.exports.run = async (bot, message, args, ops) => {
 
+  let memberToFind = message.mentions.members.first();
+
+  let color = message.member.displayHexColor || memberToFind;
 
 
     if(message.mentions.members.size == 0) {
@@ -19,7 +22,6 @@ module.exports.run = async (bot, message, args, ops) => {
    offline: "Offline/Invisible"
  };
 
-      let color1 = message.member.displayHexColor;
     const user = message.author;
     const member = message.guild.member(user);
 
@@ -30,6 +32,7 @@ module.exports.run = async (bot, message, args, ops) => {
     .addField('Joined At:', `${moment.parseZone(message.guild.members.find('id', message.author.id).joinedAt).locale('en').format('dd, DD-MM-YYYY, HH:mm')}`)
     .addField(`Roles[${message.member.roles.size - 1}]`, `${message.member.roles.map(r => `<@&${r.id}>`).slice(1).join("  |  ")}`)
     .addField('Status:', status[user.presence.status])
+     .addField('Color:', color)
     .addField('Bot?', bit[message.author.bot])
     .addField('Game:', `${message.author.presence.game ? message.author.presence.game : 'Nothing'}`)
     .setFooter(`ID ${message.author.id}`)
@@ -54,8 +57,6 @@ module.exports.run = async (bot, message, args, ops) => {
             offline: "Offline/Invisible"
      };
 
-          let memberToFind = message.mentions.members.first();
-          let color = memberToFind.displayHexColor;
 
 
           let embed = new Discord.RichEmbed()
@@ -65,6 +66,7 @@ module.exports.run = async (bot, message, args, ops) => {
         .addField('Joined At:', `${moment.parseZone(message.guild.members.find('id', memberToFind.id).joinedAt).locale('en').format('dd, DD-MM-YYYY, HH:mm')}`)
         .addField(`Roles[${memberToFind.roles.size - 1}]`, `${memberToFind.roles.map(r => `<@&${r.id}>`).slice(1).join("  |  ")}`)
         .addField('Status:', status1[memberToFind.presence.status])
+        .addField('Color:', color)
         .addField('Bot?', bit1[memberToFind.user.bot])
         .addField('game:', `${memberToFind.presence.game ? memberToFind.presence.game : 'Nothing'}`)
         .setFooter(`ID ${memberToFind.id}`)

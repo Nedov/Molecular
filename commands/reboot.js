a92aa1c53d4396b5430cefc7c39da0970cb825e3
funelnnysub <kil.oil@yandex.ru>
2018-11-27 18:52:12 +0300

    Create reboot.js

---
+++ commands/reboot.js
@@ -0,0 +1,30 @@
+const {RichEmbed} = require("discord.js"); // [package required: discord.js]
+exports.run = async (bot, message, args, level) => {
+  // EMBED
+  let embed = new RichEmbed()
+  .setColor("#ff1d00")
+  .setTitle("Bot is shutting down!")
+  await message.channel.send(embed); // send the embed
+  // unload all commands before shutting down
+
+  // you can always leave out this code // (cmd part)
+  bot.commands.forEach( async cmd => {
+    await bot.unloadCommand(cmd);
+  }); // end of cmd function
+
+  // shut down the bot
+  process.exit(1);
+}; // end of code
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["boot off", "shutdown"]
+};
+
+exports.help = {
+  name: "reboot",
+  category: "Owner",
+  description: "Shuts down the bot",
+  usage: "reboot"
+};

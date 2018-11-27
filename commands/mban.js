5e9c93c18c6c89d64aefb17c61ef21e6744b04a3
funelnnysub <kil.oil@yandex.ru>
2018-11-27 20:40:52 +0300

    Update mban.js

--- commands/mban.js
+++ commands/mban.js
@@ -27,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
       let banEmbed = new Discord.RichEmbed()
         .setDescription("Ban")
         .setColor("#bc0000")
-        .addField("Banned User", `${bUser} with ID ${bUser.id}`)
+        .addField("Banned User", `<@${bUser}> with ID ${bUser}`)
         .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
         .addField("Banned In", message.channel)
         .addField("Time", moment().locale('en').format('LLLL'))
